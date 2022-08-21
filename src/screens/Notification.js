c864db168207ce486ff50d9a7f9f2138ce432c7a
Tidbits JS <tidbitsjs@gmail.com>
2022-08-21 19:58:43 +0530

    notification screen refactor

--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, SafeAreaView, FlatList, StatusBar } from "react-native";
-import { NotificationCard } from "../components";
 
+import { NotificationCard } from "../components";
 import { COLORS, FONTS, notifications } from "../constants";
 
 const Notification = () => {
@@ -9,25 +9,25 @@ const Notification = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
 
-      <View style={{ flex: 1, padding: 20 }}>
-        <Text
-          style={{
-            fontFamily: FONTS.regular,
-            color: COLORS.tertiary,
-            fontSize: 25,
-          }}
-        >
-          Notifications
-        </Text>
-
-        <View style={{ marginTop: 50 }}>
-          <FlatList
-            data={notifications}
-            renderItem={({ item }) => <NotificationCard item={item} />}
-            keyExtractor={(item) => `notification-${item.id}`}
-          />
-        </View>
-      </View>
+      <FlatList
+        data={notifications}
+        renderItem={({ item }) => <NotificationCard item={item} />}
+        keyExtractor={(item) => `notification-${item.id}`}
+        contentContainerStyle={{ padding: 20 }}
+        ListHeaderComponent={() => (
+          <View style={{ marginBottom: 50 }}>
+            <Text
+              style={{
+                fontFamily: FONTS.regular,
+                color: COLORS.tertiary,
+                fontSize: 25,
+              }}
+            >
+              Notifications
+            </Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
