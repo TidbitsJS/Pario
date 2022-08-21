ff1278b6b0e4039d1fd665252a7be5444492f4cf
Tidbits JS <tidbitsjs@gmail.com>
2022-08-21 14:10:38 +0530

    saved plan screen refactor

--- src/screens/SavedPlans.js
+++ src/screens/SavedPlans.js
@@ -1,41 +1,33 @@
 import React from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  StatusBar,
-  Image,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
-import { SavedPlanCard } from "../components";
+import { View, Text, SafeAreaView, StatusBar, FlatList } from "react-native";
 
-import { COLORS, FONTS, icons, images, savedPlans } from "../constants";
+import { COLORS, FONTS, savedPlans } from "../constants";
+import { SavedPlanCard } from "../components";
 
 const SavedPlans = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
 
-      <View style={{ flex: 1, padding: 20 }}>
-        <Text
-          style={{
-            fontFamily: FONTS.regular,
-            color: COLORS.tertiary,
-            fontSize: 25,
-          }}
-        >
-          Saved Trip Plans
-        </Text>
-
-        <View style={{ marginTop: 50 }}>
-          <FlatList
-            data={savedPlans}
-            renderItem={({ item, index }) => <SavedPlanCard card={item} />}
-            keyExtractor={(item) => `saved-card-${item.name}`}
-          />
-        </View>
-      </View>
+      <FlatList
+        data={savedPlans}
+        renderItem={({ item }) => <SavedPlanCard card={item} />}
+        keyExtractor={(item) => `saved-card-${item.name}`}
+        contentContainerStyle={{ padding: 20 }}
+        ListHeaderComponent={() => (
+          <View style={{ marginBottom: 50 }}>
+            <Text
+              style={{
+                fontFamily: FONTS.regular,
+                color: COLORS.tertiary,
+                fontSize: 25,
+              }}
+            >
+              Saved Trip Plans
+            </Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
